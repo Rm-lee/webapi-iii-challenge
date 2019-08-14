48fa33ad2fcc733e7fb68cd596a9349bc3e3c68f
Roger Lee <rmlee0003@gmail.com>
2019-08-13 21:06:48 -0400

    added validtateUserId middleware function

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,7 @@
 const express = 'express';
-
+const users = require('./userDb')
 const router = express.Router();
+
 
 router.post('/', (req, res) => {
 
@@ -33,7 +34,24 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+ const {id} = req.params
+ users.getById(id)
+ .then(user => {
+  if (user){
+   req.user = user
+   next()
+  }
+  else{
+   res.status(400).json({
+    message: " invalid user id"
+   })
+  }
+ })
+ .catch(error => {
+  res.status(500).json({
+   message: 'error processing request'
+  })
+ })
 };
 
 function validateUser(req, res, next) {
@@ -43,5 +61,6 @@ function validateUser(req, res, next) {
 function validatePost(req, res, next) {
 
 };
+
 
 module.exports = router;
